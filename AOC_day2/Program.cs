d3429b1ffe4c605af93dd86bbd79dd674f9d9b25
Mateusz Karzek <m.karzek@cinkciarz.pl>
2020-12-02 08:39:56 +0100

    day 2

--- AOC_day2/Program.cs
+++ AOC_day2/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using static System.Console;
 
@@ -9,9 +8,27 @@ namespace AOC_day2
     {
         static void Main(string[] args)
         {
-            var nums = File.ReadAllLines("text.txt").Select(o => int.Parse(o));
+            int i = 0, j = 0;
+            var nums = File.ReadAllLines("text.txt");
+            foreach (var line in nums)
+            {
+                var splited = line.Split();
+                var numbers = splited[0].Split('-');
+                var first = int.Parse(numbers[0]);
+                var second = int.Parse(numbers[1]);
+                var symbol = splited[1].Trim(':')[0];
 
+                var qwe = splited[2].Count(o => o == symbol);
+                if (qwe >= first && qwe <= second)
+                    i++;
 
+                if ((splited[2][first - 1] == symbol && splited[2][second - 1] != symbol) || (splited[2][first - 1] != symbol && splited[2][second - 1] == symbol))
+                    j++;
+
+            }
+
+            WriteLine(i);
+            WriteLine(j);
             ReadKey();
         }
     }
