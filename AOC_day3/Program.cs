614a5c1b6137d2ef17db3998b6a5ec82d5f4c2e9
Mateusz Karzek <mateuszkarzek@gmail.com>
2020-12-03 22:54:08 +0100

    DAY 3

---
+++ AOC_day3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AOC_day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("text.txt").ToList();
+            for (int j = 0; j < lines.Count(); j++)
+            {
+                var line = lines[j];
+                for (int i = 0; i < j; i++)
+                    lines[j] += line;
+            }
+
+            uint result = Gogogo(3, 1, lines);
+            Console.WriteLine(result);
+            result *= Gogogo(1, 1, lines);
+            result *= Gogogo(5, 1, lines);
+            result *= Gogogo(7, 1, lines);
+            result *= Gogogo(1, 2, lines);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        private static uint Gogogo(int addX, int addY, List<string> lines)
+        {
+            uint counter = 0;
+
+            int x = 0;
+            for (int y = 0; y < lines.Count(); y += addY)
+            {
+                if (lines[y][x] == '#')
+                    counter++;
+                x += addX;
+            }
+            return counter;
+        }
+    }
+}
