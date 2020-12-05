5bd1ee0c9880c58cae4a8a210fd6b7f24dd3663f
Mateusz Karzek <mateuszkarzek@gmail.com>
2020-12-05 15:50:47 +0100

    Day 5

---
+++ AOC_day5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AOC_day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var seats = File.ReadAllLines(@"..\..\..\text.txt").Select(o => ConvertToInt(o.Substring(0, 7), 'F') * 8 + ConvertToInt(o.Substring(7), 'L')).OrderBy(o => o).ToList();
+
+            Console.WriteLine(seats.Max());
+
+            for (int i = seats.Min(); i <= seats.Max(); i++)
+                if (!seats.Contains(i))
+                    Console.WriteLine(i);
+
+            Console.ReadKey();
+        }
+        static int ConvertToInt(string s, char lowerChar) => Convert.ToInt32(s.Select(o => o == lowerChar ? "0" : "1").Aggregate((x, y) => x + y), 2);
+    }
+}
