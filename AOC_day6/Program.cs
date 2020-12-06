52b06e6ecc99fa08f726d2ea644c59e2aaa6fd3f
Mateusz Karzek <mateuszkarzek@gmail.com>
2020-12-06 17:12:32 +0100

    Day 6

---
+++ AOC_day6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AOC_day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter1 = 0;
+            var counter2 = 0;
+            var lines = File.ReadAllLines(@"..\..\..\text.txt");
+            var groups = new List<List<string>> { new List<string> { lines.First() } };
+            foreach (var line in lines.Skip(1))
+                if (line == "")
+                    groups.Add(new List<string>());
+                else
+                    groups.Last().Add(line);
+
+            foreach (var group in groups)
+            {
+                var answers = string.Join("", group).Distinct();
+                var answersCount = answers.Count();
+                counter1 += answersCount;
+
+                foreach (var answer in answers)
+                    if (group.All(o => o.Contains(answer)))
+                        counter2++;
+
+            }
+            Console.WriteLine($"{counter1}{Environment.NewLine}{counter2}");
+            Console.ReadKey();
+        }
+    }
+}
